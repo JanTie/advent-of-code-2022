b9fb407d4d427dd6c255bada143bde51d7bd2a3b
Jan Tiedemann <jan.tiedemann@ffwagency.com>
2022-12-06 12:05:41 +0100

    Day06

---
+++ src/Day06.kt
@@ -0,0 +1,23 @@
+fun main() {
+    fun String.findMarkerIndex(length: Int) = this
+            .windowed(length)
+            .indexOfFirst { it.toList().distinct().size == length } + length // add length because we skipped length indices due to window
+
+    fun part1(input: List<String>): Int {
+        return input[0].findMarkerIndex(4)
+    }
+
+    fun part2(input: List<String>): Int {
+        return input[0].findMarkerIndex(14)
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day06_test")
+    val input = readInput("Day06")
+
+    check(part1(testInput) == 7)
+    println(part1(input))
+
+    check(part2(testInput) == 19)
+    println(part2(input))
+}
